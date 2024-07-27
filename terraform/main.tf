7691f94266455e9d7aa04839a48493293f56e80d
mo1911997 <mmd26.aka@gmail.com>
2024-07-27 22:23:44 +0530

    update: fixed minor tf issues

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,30 @@
-# Artifact Registry Configuration
-# resource "google_artifact_registry_repository" "nestjs-app" {
-#   location      = var.region
-#   repository_id = "nestjs-app"
-#   description   = "Artifact registry for storing docker artifacts"
-#   format        = "DOCKER"
-# }
+#Artifact Registry Configuration
+resource "google_artifact_registry_repository" "nestjs-app" {
+  location      = var.region
+  repository_id = "nestjs-app"
+  description   = "Artifact registry for storing docker artifacts"
+  format        = "DOCKER"
+}
 
-# # Staging GKE Cluster
-# resource "google_container_cluster" "staging" {
-#   name                = "staging"
-#   location            = var.region
-#   enable_autopilot    = true
-#   deletion_protection = false
-# }
+# Staging GKE Cluster
+resource "google_container_cluster" "staging" {
+  name                = "staging"
+  location            = var.region
+  enable_autopilot    = true
+  deletion_protection = false
+}
 
-# # Production GKE Cluster
-# resource "google_container_cluster" "production" {
-#   name                = "production"
-#   location            = var.region
-#   enable_autopilot    = true
-#   deletion_protection = false
-# }
+# Production GKE Cluster
+resource "google_container_cluster" "production" {
+  name                = "production"
+  location            = var.region
+  enable_autopilot    = true
+  deletion_protection = false
+}
 
 # Cloud Build configuration
 resource "google_cloudbuild_trigger" "nestjs-app-trigger" {
   name = "nestjs-app-trigger"
-  # trigger_template {
-  #   branch_name = "master"
-  #   repo_name   = "nestjs-app-gke"
-  # }
   github {
     owner = "mmdcloud"
     name  = "nestjs-app-gke"
@@ -41,39 +37,39 @@ resource "google_cloudbuild_trigger" "nestjs-app-trigger" {
   filename = "cloudbuild.yaml"
 }
 
-# Cloud Deploy Delivery Pipeline
-# resource "google_clouddeploy_delivery_pipeline" "nestjs-app" {
-#   name        = "nestjs-app"
-#   location    = var.region
-#   description = "Nest.js App Deployment Pipeline"
-#   serial_pipeline {
-#     stages {
-#       target_id = "staging"
-#       profiles  = ["staging"]
-#     }
-#     stages {
-#       target_id = "production"
-#       profiles  = ["production"]
-#     }
-#   }
-# }
+#Cloud Deploy Delivery Pipeline
+resource "google_clouddeploy_delivery_pipeline" "nestjs-app" {
+  name        = "nestjs-app"
+  location    = var.region
+  description = "Nest.js App Deployment Pipeline"
+  serial_pipeline {
+    stages {
+      target_id = "staging"
+      profiles  = ["staging"]
+    }
+    stages {
+      target_id = "production"
+      profiles  = ["production"]
+    }
+  }
+}
 
-# # Cloud Deploy Target
-# resource "google_clouddeploy_target" "staging" {
-#   name        = "staging"
-#   location    = var.region
-#   description = "Staging environment"
-#   gke {
-#     cluster = "projects/${var.project_id}/locations/${var.region}/clusters/staging"
-#   }
-# }
+# Cloud Deploy Target
+resource "google_clouddeploy_target" "staging" {
+  name        = "staging"
+  location    = var.region
+  description = "Staging environment"
+  gke {
+    cluster = "projects/${var.project_id}/locations/${var.region}/clusters/staging"
+  }
+}
 
-# # Cloud Deploy Target
-# resource "google_clouddeploy_target" "production" {
-#   name        = "production"
-#   location    = var.region
-#   description = "Production environment"
-#   gke {
-#     cluster = "projects/${var.project_id}/locations/${var.region}/clusters/production"
-#   }
-# }
+# Cloud Deploy Target
+resource "google_clouddeploy_target" "production" {
+  name        = "production"
+  location    = var.region
+  description = "Production environment"
+  gke {
+    cluster = "projects/${var.project_id}/locations/${var.region}/clusters/production"
+  }
+}
